ec4491810d983659136601cead6325ffbec84b27
muhammedfaiz <faizmuhammed342@gmail.com>
2024-03-12 15:30:39 +0530

    Atlas added

--- config.js
+++ config.js
@@ -1,23 +1,22 @@
 const mongoose = require("mongoose");
 
 module.exports = {
-  mongooseSetup: () => {
+  mongooseSetup: async () => {
     // mongoose connection establishment
-    const uri = process.env.URI;
-    mongoose.connect(uri);
+    try {
+      const uri = process.env.URI;
+ 
+      mongoose.connect(uri);  
+  
+      mongoose.connection.on("connected", () => {
+        console.log("connected to Mongodb");
+      });
 
-    const db = mongoose.connection;
-
-    db.on("connected", () => {
-      console.log("connected to Mongodb");
-    });
-
-    db.on("error", (err) => {
-      console.error("Error connecting to MongoDB:", err);
-    });
-
-    db.on("disconnected", () => {
-      console.log("Disconnected from MongoDB");
-    });
+      mongoose.connection.on("disconnected", () => {
+        console.log("Disconnected from MongoDB");
+      });
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
